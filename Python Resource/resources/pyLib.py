ed4c07f8cbce6f88b9cb83ea7c39eb6366887eba
Jared Mountan <c-jmountan@copado.com>
2022-06-24 15:58:21 -0400

    added logger statements

--- Python Resource/resources/pyLib.py
+++ Python Resource/resources/pyLib.py
@@ -15,19 +15,24 @@ class pyLib():
 
     def set_base(self, number):
         baseNum = number
+        logger.warn("Base set to %s" %(number))
     
     def squareroot_base(self):
         baseNum = numpy.sqrt(baseNum)
+        logger.warn("Base set to %s" %(baseNum))
         return baseNum
 
     def double_base(self):
         baseNum = numpy.multiply(baseNum, 2.0)
+        logger.warn("Base set to %s" %(baseNum))
         return baseNum
     
     def halve_base(self):
         baseNum = numpy.divide(baseNum, 2.0)
+        logger.warn("Base set to %s" %(baseNum))
         return baseNum
 
     def sum_base(self, number):
         baseNum = numpy.add(baseNum, nummber)
+        logger.warn("Base set to %s" %(baseNum))
         return baseNum
