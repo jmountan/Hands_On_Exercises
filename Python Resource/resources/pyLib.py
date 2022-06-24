a43243529773c9c693fc84b765d440612040644e
Jared Mountan <c-jmountan@copado.com>
2022-06-24 16:07:11 -0400

    adjusted away from variables

--- Python Resource/resources/pyLib.py
+++ Python Resource/resources/pyLib.py
@@ -3,8 +3,6 @@ import numpy
 
 class pyLib():
 
-    baseNum = 1000
-
     def print_message(self, message):
 
         """Prints a custom message to log
@@ -12,27 +10,23 @@ class pyLib():
         """
 
         logger.warn(message)
-
-    def set_base(self, number):
-        baseNum = number
-        logger.warn("Base set to %s" %(number))
     
-    def squareroot_base(self):
-        baseNum = numpy.sqrt(baseNum)
-        logger.warn("Base set to %s" %(baseNum))
-        return baseNum
+    def squareroot(self, baseNum):
+        newNum = numpy.sqrt(baseNum)
+        logger.warn("%s calculated to %s" %(baseNum,newNum))
+        return newNum
 
-    def double_base(self):
-        baseNum = numpy.multiply(baseNum, 2.0)
-        logger.warn("Base set to %s" %(baseNum))
-        return baseNum
+    def double(self, baseNum):
+        newNum = numpy.multiply(baseNum, 2.0)
+        logger.warn("%s calculated to %s" %(baseNum,newNum))
+        return newNum
     
-    def halve_base(self):
-        baseNum = numpy.divide(baseNum, 2.0)
-        logger.warn("Base set to %s" %(baseNum))
-        return baseNum
+    def halve(self, baseNum):
+        newNum = numpy.divide(baseNum,2.0)
+        logger.warn("%s calculated to %s" %(baseNum,newNum))
+        return newNum
 
-    def sum_base(self, number):
-        baseNum = numpy.add(baseNum, nummber)
-        logger.warn("Base set to %s" %(baseNum))
-        return baseNum
+    def sum(self, baseNum, number):
+        newNum = numpy.add(baseNum, number)
+        logger.warn("%s calculated to %s" %(baseNum,newNum))
+        return newNum
