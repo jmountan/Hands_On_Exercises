d7034d4592bc8d3131d32418e5db10926da82723
Jared Mountan <c-jmountan@copado.com>
2022-06-24 15:50:15 -0400

    fleshed out .py

--- Python Resource/resources/pyLib.py
+++ Python Resource/resources/pyLib.py
@@ -1,6 +1,9 @@
 from robot.api import logger
+import numpy
 
 class pyLib():
+
+    baseNum = 1000
 
     def print_message(self, message):
 
@@ -9,3 +12,22 @@ class pyLib():
         """
 
         logger.warn(message)
+
+    def set_base(self, number):
+        baseNum = number
+    
+    def squareroot_base(self):
+        baseNum = numpy.sqrt(baseNum)
+        return baseNum
+
+    def double_base(self):
+        baseNum = numpy.multiply(baseNum, 2.0)
+        return baseNum
+    
+    def halve_base(self):
+        baseNum = numpy.divide(baseNum, 2.0)
+        return baseNum
+
+    def sum_base(self, number):
+        baseNum = numpy.add(baseNum, nummber)
+        return baseNum
