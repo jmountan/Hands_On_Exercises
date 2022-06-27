9c08d0924293b7aae4dc9baa68e5af81b611fe43
Jared Mountan <c-jmountan@copado.com>
2022-06-27 15:20:13 -0400

    type casting to floats

--- Python Resource/resources/pyLib.py
+++ Python Resource/resources/pyLib.py
@@ -11,22 +11,22 @@ class pyLib():
 
         logger.warn(message)
     
-    def squareroot(self, float(baseNum)):
-        newNum = numpy.sqrt(baseNum)
+    def squareroot(self, baseNum):
+        newNum = numpy.sqrt(float(baseNum))
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
 
-    def double(self, float(baseNum)):
-        newNum = numpy.multiply(baseNum, 2.0)
+    def double(self, baseNum):
+        newNum = numpy.multiply(float(baseNum), 2.0)
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
     
-    def halve(self, float(baseNum)):
-        newNum = numpy.divide(baseNum,2.0)
+    def halve(self, baseNum):
+        newNum = numpy.divide(float(baseNum),2.0)
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
 
-    def sum(self, float(baseNum), float(number)):
-        newNum = numpy.add(baseNum, number)
+    def sum(self, baseNum, number):
+        newNum = numpy.add(float(baseNum), float(number))
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
