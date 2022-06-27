7e577cc4c86880a625e3d68e4e8ec2495c55ae2a
Jared Mountan <c-jmountan@copado.com>
2022-06-27 15:52:23 -0400

    Added messages for examples

--- Python Resource/resources/pyLib.py
+++ Python Resource/resources/pyLib.py
@@ -12,21 +12,25 @@ class pyLib():
         logger.warn(message)
     
     def squareroot(self, baseNum):
+        logger.warn("Taking Squareroot of %s" %(baseNum))
         newNum = numpy.sqrt(float(baseNum))
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
 
     def double(self, baseNum):
+        logger.warn("Doubling the value of %s" %(baseNum))
         newNum = numpy.multiply(float(baseNum), 2.0)
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
     
     def halve(self, baseNum):
+        logger.warn("Halving the value of %s" %(baseNum))
         newNum = numpy.divide(float(baseNum),2.0)
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
 
     def sum(self, baseNum, number):
+        logger.warn("Summming the values of %s and %s" %(baseNum,newNum))
         newNum = numpy.add(float(baseNum), float(number))
         logger.warn("%s calculated to %s" %(baseNum,newNum))
         return newNum
