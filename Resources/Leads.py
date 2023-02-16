a70d0c6871235d0506c19fe738b7e82412c40e59
Jared Mountan <c-jmountan@copado.com>
2023-02-16 14:31:48 -0500

    Swapped out to be one list

--- Resources/Leads.py
+++ Resources/Leads.py
@@ -1,5 +1,5 @@
-FirstNames=[John,Evan,Neil,Paul,Benjamin,Kimberly]
-LastNames=[Smith,Powell,Edmunds,Wilson,Carr,Henderson]
-Companies=[Salesforce,Google,Tesla,Copado,Apple,Microsoft]
-PhoneNums=[+15736562741345,+16427334108654,+13331758617745,+10821340457659,+11070630452845,+10531945374546]
-Websites=[https://salesforce.com,https://google.com,https://tesla.com,https://copado.com,https://deloitte.com,https://accenture.com]
+LeadList=[[John,Evan,Neil,Paul,Benjamin,Kimberly],\
+         [Smith,Powell,Edmunds,Wilson,Carr,Henderson],\
+         [Salesforce,Google,Tesla,Copado,Apple,Microsoft],\
+         [+15736562741345,+16427334108654,+13331758617745,+10821340457659,+11070630452845,+10531945374546],\
+         [https://salesforce.com,https://google.com,https://tesla.com,https://copado.com,https://deloitte.com,https://accenture.com]]
