eeea857fc970fd9ca41a956be92db4e7c864b36d
Jared Mountan <c-jmountan@copado.com>
2023-02-16 14:53:15 -0500

    removed linebreaks

--- Resources/Leads.py
+++ Resources/Leads.py
@@ -1,3 +1,1 @@
-LeadList=[[John,Smith,Salesforce,+15736562741345,https://salesforce.com],\
-         [Evan,Powell,Google,+16427334108654,https://google.com],\
-         [Paul,Edmunds,Copado,+13331758617745,https://copado.com]]
+LeadList=[[John,Smith,Salesforce,+15736562741345,https://salesforce.com],[Evan,Powell,Google,+16427334108654,https://google.com],[Paul,Edmunds,Copado,+13331758617745,https://copado.com]]
