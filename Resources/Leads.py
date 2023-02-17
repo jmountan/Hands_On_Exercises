5e6a153abf72681837750e23788bfd29a59b7a79
Jared Mountan <c-jmountan@copado.com>
2023-02-17 13:05:34 -0500

    Separated variable file into lists

--- Resources/Leads.py
+++ Resources/Leads.py
@@ -1,1 +1,5 @@
-LeadList=[[John,Smith,Salesforce,+15736562741345,https://salesforce.com],[Evan,Powell,Google,+16427334108654,https://google.com],[Paul,Edmunds,Copado,+13331758617745,https://copado.com]]
+FirstNames=[John,Evan,Paul]
+LastNames=[Smith,Powell,Edmunds]
+Companies=[Salesforce,Google,Copado]
+PhoneNums=[+15736562741345,+16427334108654,+13331758617745]
+Websites=[https://salesforce.com,https://google.com,https://copado.com]
